9ccec93f88dfdbf113254e75ef5ef2b01e51b14a
victoria655 <karonjivictoria@gmail.com>
2025-03-17 11:18:02 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+ function saturdayFun (activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(duty='go to the office'
+      ){
+  return `This Monday, I will ${duty}.`
+}
+console.log(mondayWork())
+
+function wrapAdjective(flair) {
+    return function(adjective='special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };                                                                                                                                  
+  }
+  
